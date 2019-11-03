37e2175e5a682f73431f650956a29f73f894191a
Jonathan Lutz <jlutz.110@gmail.com>
2019-11-03 15:35:44 +0700

    Added starter files

---
+++ js/script.js
@@ -0,0 +1,61 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array  
+  Must include: quote, source, citation (optional), year(optional)
+***/
+var quotes = [
+	{quote: "There is no substitute for hard work.",
+	source: "Thomas Edison",
+	}
+
+	{quote: "I find that the harder I work, the more luck I seem to have.",
+	source: "Thomas Jefferson",
+	}
+
+	{quote: "To succeed, work hard, never give up, and above all, cherish a magnificent obsession.",
+	source: "Walt Disney",
+	}
+
+	{quote: "Be humble, be hungry, and always be the hardest worker in the room.",
+	source: "Dwayne 'The Rock' Johnson",
+	}
+
+	{quote: "Great things come from hard work and perserverance.  No excuses.",
+	source: "Kobe Bryant",
+	}
+
+	{quote: "The road to success comes through hard work, determination, and sacrafice",
+	source: "Dolzinski",
+	}
+
+]
+
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE OF CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
